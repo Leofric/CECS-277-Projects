34e5152eac556bcff5193c67d7cab228cbe5fec7
Alex Berthon <Alexjberthon@hotmail.com>
2016-11-07 13:52:58 -0800

    Added all methods

--- src/edu/project/six/Employee.java
+++ src/edu/project/six/Employee.java
@@ -1,31 +1,34 @@
 package edu.project.six;
 
-public class Employee {
-private String firstName;
-private String lastName;
-private int ID; //unique
+public class Employee implements Comparable<Employee> {
+	private String firstName;
+	private String lastName;
+	private int ID; // unique
 
-public Employee(){
-	firstName = "fub";
-	lastName = "city";
-	ID = 0;
-}
+	public Employee() {
+		firstName = "fub";
+		lastName = "city";
+		ID = 0;
+	}
 
-public Employee(String first, String last, int ID){
-	firstName = first;
-	lastName = last;
-	this.ID = ID;
-}
+	public Employee(String first, String last, int ID) {
+		firstName = first;
+		lastName = last;
+		this.ID = ID;
+	}
 
-public String print(){
-	return firstName+lastName+ID;
-}
+	public String print() {
+		return firstName + " " + lastName + " " + ID + " ";
+	}
 
-public int hashCode(){
-	final int HASH_MULTIPLIER = 29;
-	int h = HASH_MULTIPLIER * firstName.hashCode() + lastName.hashCode();
-	h = HASH_MULTIPLIER * h + ((Integer)ID).hashCode();
-	return h;
+	public int hashCode() {
+		final int HASH_MULTIPLIER = 29;
+		int h = HASH_MULTIPLIER * firstName.hashCode() + lastName.hashCode();
+		h = HASH_MULTIPLIER * h + ((Integer) ID).hashCode();
+		return h;
+	}
+
+	public int compareTo(Employee a) {
+		return this.lastName.compareTo(a.lastName);
 	}
 }
-
--- src/edu/project/six/EmployeeDatabase.java
+++ src/edu/project/six/EmployeeDatabase.java
@@ -1,6 +1,7 @@
 package edu.project.six;
 
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
@@ -9,28 +10,26 @@ import java.util.TreeMap;
 public class EmployeeDatabase {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		Map<Integer, Employee> employeeMap = new TreeMap<Integer, Employee>();
 		Map<Employee, String> performanceMap = new HashMap<Employee, String>();
-
-		employeeMap.put(0, new Employee("bob", "dole", 0));
-		performanceMap.put(employeeMap.get(0), "BAD");
 
 		boolean run = true;
 		int choice;
 		while (run) {
-			choice = printMenuAndGetChoice();
+			choice = printMenuAndGetChoice(in);
 			switch (choice) {
 			case 1:
-				addEmployee(employeeMap, performanceMap);
+				addEmployee(employeeMap, performanceMap, in);
 				break;
 			case 2:
-				removeEmployee(employeeMap, performanceMap);
+				removeEmployee(employeeMap, performanceMap, in);
 				break;
 			case 3:
-				printPerformance(employeeMap, performanceMap);
+				printPerformance(employeeMap, performanceMap, in);
 				break;
 			case 4:
-				modifyPerformance(employeeMap, performanceMap);
+				modifyPerformance(employeeMap, performanceMap, in);
 				break;
 			case 5:
 				run = false;
@@ -40,58 +39,80 @@ public class EmployeeDatabase {
 
 	}
 
-	public static int printMenuAndGetChoice() {
-		Scanner in = new Scanner(System.in);
-		System.out.println("Press 1 to add an employee");
-		System.out.println("Press 2 to remove an employee");
-		System.out.println("Press 3 to print performance");
-		System.out.println("Press 4 to modify performance");
-		System.out.println("Press 5 to quit");
-		int choice = in.nextInt();
+	public static int printMenuAndGetChoice(Scanner in) {
+		boolean InvalidInput = true;
+		int choice = 5;
+		while (InvalidInput) {
+			System.out.println("Press 1 to add an employee");
+			System.out.println("Press 2 to remove an employee");
+			System.out.println("Press 3 to print performance");
+			System.out.println("Press 4 to modify performance");
+			System.out.println("Press 5 to quit\n");
+			try {
+				choice = in.nextInt();
+				InvalidInput = false;
+			} catch (InputMismatchException ex) {
+				System.out.println("-INVALID INPUT-\n");
+				in.nextLine();
+			}
+		}
 		return choice;
 	}
 
-	public static void addEmployee(Map<Integer, Employee> e, Map<Employee, String> ep) {
-		Scanner in = new Scanner(System.in);
-		
+	public static void addEmployee(Map<Integer, Employee> e, Map<Employee, String> ep, Scanner in) {
 		System.out.println("Please enter the first name of the employee");
+		in.nextLine();
 		String first = in.nextLine();
 		System.out.println("Please enter the last name of the employee");
 		String last = in.nextLine();
-		System.out.println("Please enter the ID number of the employee");
-		int ID = in.nextInt();
+		boolean checked = false;
+		int ID = -1;
+		while (!checked) {
+			System.out.println("Please enter the ID number of the employee");
+			try {
+				ID = in.nextInt();
+				if (e.containsKey(ID)) {
+					System.out.println("An employee with that ID number already exists\n");
+				} else
+					checked = true;
+			} catch (InputMismatchException ex) {
+				System.out.println("-INVALID ENTRY-\n");
+				in.nextLine();
+			}
+		}
 		Employee newGuy = new Employee(first, last, ID);
 		e.put(ID, newGuy);
 		System.out.println("Please enter the performance of the employee");
-		ep.put(newGuy, in.nextLine());		
+		in.nextLine();
+		ep.put(newGuy, in.nextLine());
 	}
 
-	public static void removeEmployee(Map<Integer, Employee> e, Map<Employee, String> ep) {
-		Scanner in = new Scanner(System.in);
+	public static void removeEmployee(Map<Integer, Employee> e, Map<Employee, String> ep, Scanner in) {
 		System.out.println("Enter the ID number of the employee you wish to remove");
 		int ID = in.nextInt();
-		e.remove(ID);
-		ep.remove(e.get(ID));
-		System.out.println("Removed " + e.get(ID).print());
-		in.close();
+		if (e.containsKey(ID)) {
+			System.out.println("Removed " + e.get(ID).print() + "\n");
+			e.remove(ID);
+			ep.remove(e.get(ID));
+		} else
+			System.err.println("-ID number does not exist-\n");
 	}
 
-	public static void printPerformance(Map<Integer, Employee> e, Map<Employee, String> ep) {
+	public static void printPerformance(Map<Integer, Employee> e, Map<Employee, String> ep, Scanner in) {
 		Set<Integer> fub = e.keySet();
 		for (Integer i : fub) {
-			System.out.println(e.get(i).print() + "Performance : " + ep.get(e.get(i)));
+			System.out.println(e.get(i).print() + "Performance: " + ep.get(e.get(i)));
 		}
+		System.out.println();
 	}
 
-	public static void modifyPerformance(Map<Integer, Employee> e, Map<Employee, String> ep) {
-		Scanner in = new Scanner(System.in);
+	public static void modifyPerformance(Map<Integer, Employee> e, Map<Employee, String> ep, Scanner in) {
 		System.out.println("Please enter the ID number of the employee you wish to edit");
 		int ID = in.nextInt();
 		System.out.println("Please enter the performance update");
 		String performance = in.nextLine();
 		Employee guy = e.get(ID);
 		ep.replace(guy, performance);
-		System.out.println("Changed" + guy.print() + "performance to : " + performance);
-		in.close();
+		System.out.println("Changed " + guy.print() + "performance to : " + performance + "\n");
 	}
 }
