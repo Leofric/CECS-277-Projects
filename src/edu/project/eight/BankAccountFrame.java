f73f27e87aac11e908219e520be7c1441a05d438
Alex Berthon <Alexjberthon@hotmail.com>
2016-12-04 16:32:03 -0800

    Project 8

---
+++ src/edu/project/eight/BankAccountFrame.java
@@ -0,0 +1,17 @@
+package edu.project.eight;
+
+import javax.swing.JFrame;
+/**
+ * This is the main window
+ * @author Alex Berthon
+ *
+ */
+public class BankAccountFrame extends JFrame {
+
+	public BankAccountFrame(BankAccount account) {
+		setTitle("Bank Account");
+		setBounds(267, 200, 425, 100);
+		setResizable(false);
+		this.add(new BankAccountPanel(account));
+	}
+}
